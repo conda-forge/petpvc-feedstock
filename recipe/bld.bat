c2effe0422391399805d14e2b2e84429ddc11543
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-09-07 07:10:35 +0200

    Revamp build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,27 @@
 setlocal EnableDelayedExpansion
 
-mkdir build
-cd build
-
-cmake -GNinja ^
-    -DCMAKE_BUILD_TYPE:STRING=Release ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-    ..
+:: config
+cmake %CMAKE_ARGS% ^
+    	-D CMAKE_BUILD_TYPE:STRING=Release ^
+    	-D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+    	-D CMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
+    	-G Ninja ^
+	-B build ^
+	-S %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build .
+:: build
+cmake --build build --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
 
-ctest --extra-verbose --output-on-failure .
+:: install
+cmake --install build
 if errorlevel 1 exit 1
 
-cmake --install .
-if errorlevel 1 exit 1
-
+:: post-install
 rmdir /s /q "%LIBRARY_PREFIX%\parc"
+if errorlevel 1 exit 1
+
+:: test
+ctest --test-dir build --extra-verbose --output-on-failure
+if errorlevel 1 exit 1
